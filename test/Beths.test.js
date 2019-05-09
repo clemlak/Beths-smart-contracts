8795462edd8522e01fb4df08096ecb299de0e204
Clement Lakhal <clementlakhal@gmail.com>
2019-05-09 02:17:06 +0200

    Update test

--- test/Beths.test.js
+++ test/Beths.test.js
@@ -1,22 +1,20 @@
 /* eslint-env node, mocha */
 /* global artifacts, contract, it, assert */
 
+const TestToken = artifacts.require('TestToken');
 const Beths = artifacts.require('Beths');
 
-let instance;
+let testToken;
+let beths;
 
 contract('Beths', (accounts) => {
-  it('Should deploy an instance of the Beths contract', () => Beths.deployed()
-    .then((contractInstance) => {
-      instance = contractInstance;
+  it('Should deploy an instance of the TestToken contract', () => TestToken.deployed()
+    .then((instance) => {
+      testToken = instance;
     }));
 
-  it('Should set the number', () => instance.setNumber(2, {
-    from: accounts[0],
-  }));
-
-  it('Should get the number', () => instance.getNumber()
-    .then((number) => {
-      assert.equal(number.toNumber(), 2, 'Number is wrong!');
+  it('Should deploy an instance of the Beths contract', () => Beths.deployed()
+    .then((instance) => {
+      beths = instance;
     }));
 });
