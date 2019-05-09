b5362d3f40f5c73896cc1701e95097f5c0adc294
Clement Lakhal <clementlakhal@gmail.com>
2019-05-09 02:17:22 +0200

    Update

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,8 +2,10 @@
 /* global artifacts */
 
 const Beths = artifacts.require('Beths');
+const TestToken = artifacts.require('TestToken');
 
 function deployContracts(deployer) {
+  deployer.deploy(TestToken);
   deployer.deploy(Beths);
 }
 
