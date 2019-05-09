4c755543e84280f4a6e4220cc0fe608741f06ea8
Clement Lakhal <clementlakhal@gmail.com>
2019-05-09 09:01:17 +0200

    Update deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,18 @@
 /* eslint-env node */
 /* global artifacts */
 
+const UsernameManager = artifacts.require('UsernameManager');
 const Beths = artifacts.require('Beths');
 const TestToken = artifacts.require('TestToken');
 
-function deployContracts(deployer) {
-  deployer.deploy(TestToken);
-  deployer.deploy(Beths);
+function deployContracts(deployer, network) {
+  if (network === 'development') {
+    deployer.deploy(TestToken)
+      .then(() => deployer.deploy(UsernameManager))
+      .then(() => deployer.deploy(Beths));
+  } else {
+    deployer.deploy(Beths);
+  }
 }
 
 module.exports = deployContracts;
